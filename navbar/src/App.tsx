7a5843c2c7098b280612ec2448bcc06cc377404a
Vinay kumar <vinayrahar1991@gmail.com>
2020-06-22 15:00:12 +0530

    Cleanup App component

--- navbar/src/App.tsx
+++ navbar/src/App.tsx
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import GridTest from './components/GridTest/GridTest';
-import GridBootTest from './components/GridTest/GridBootTest';
-import Content from './components/Content/Content';
 
 function App() {
-  const [selectedLink, setSelectedLink] = useState('');
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Content selectedLink={selectedLink}></Content>
     </div>
   );
 }
